f464dd828117aecba22bde4705a8210d4bc3a25d
Rishi Pradeep <rishipradeep@gmail.com>
2024-12-23 23:50:29 +0530

    Saved TestController

--- src/main/java/com/connectbundle/connect/controller/TestController.java
+++ src/main/java/com/connectbundle/connect/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.connectbundle.connect.configs.BaseResponse;
+import com.connectbundle.connect.dto.BaseResponse;
 
 @RestController
 @RequestMapping("/api/test")
